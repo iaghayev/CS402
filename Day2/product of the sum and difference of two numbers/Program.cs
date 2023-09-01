00c2a546bc193fa05cce5e1089d743106e9a1936
Ismayil Aghayev <adayev.1998@gmail.com>
2023-09-01 17:47:09 +0400

    Day2

---
+++ Day2/product of the sum and difference of two numbers/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int num1 = 7;
+        int num2 = 3;
+
+        int result = ProductOfSumAndDifference(num1, num2);
+
+        Console.WriteLine($"The product of the sum and difference of {num1} and {num2} is: {result}");
+    }
+
+    public static int ProductOfSumAndDifference(int a, int b)
+    {
+        int sum = a + b;
+        int difference = a - b;
+        int product = sum * difference;
+
+        return product;
+    }
+}
+
---
+++ Day2/sum of two numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        int num1 = 5;
+        int num2 = 3;
+        int result = AddNumbers(num1, num2);
+        Console.WriteLine($"The sum of {num1} and {num2} is: {result}");
+    }
+
+    // Function to add two numbers
+    public static int AddNumbers(int a, int b)
+    {
+        return a + b;
+    }
+}
