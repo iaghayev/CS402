a7d8412c027dc3e20d6725d44df93ff4b77222cf
Aghayev Ismayil <ismayilua@code.local>
2023-09-17 13:57:59 +0400

    Project

--- sql and js/app.js
+++ sql and js/app.js
@@ -1,34 +1,59 @@
 const express = require('express');
 const mysql = require('mysql2');
 const bodyParser = require('body-parser');
+const { v4: uuidv4 } = require('uuid'); // Import the UUID library
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'Salam123',
-  database: 'socialmedia'
+  host: 'bapqcxs4rj7ja1hh5ccs-mysql.services.clever-cloud.com',
+  user: 'uij3zyohd2m8plgn',
+  password: 'G031iGC5CxJMyjjdaiM1',
+  database: 'bapqcxs4rj7ja1hh5ccs'
 });
 
-db.query('SELECT 1', (err) => {
+db.getConnection((err, connection) => {
   if (err) {
     console.error('Error connecting to MySQL:', err);
-  } else {
-    console.log('Connected to MySQL database');
+    throw err;
   }
+  console.log('Connected to MySQL database');
+  connection.release();
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 
+// Serve the HTML form for adding a user
+app.get('/addUser', (req, res) => {
+  // Render the "Add User" form
+  res.render('addUserForm');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
+// Handle the HTML form submission for adding a user
+app.post('/addUser', (req, res) => {
+  const { name, surname, password } = req.body;
 
+  // Insert the user into the database
+  const sql = 'INSERT INTO users (AD, SOYAD, PASSWORDS) VALUES (?, ?, ?)';
+  const values = [name, surname, password];
+
+  db.query(sql, values, (err, result) => {
+    if (err) {
+      console.error('Error inserting user:', err);
+      return res.status(500).send('Error inserting user');
+    }
+    console.log('User inserted');
+    res.redirect('/');
+  });
+});
+
+// API route to fetch all users
 app.get('/api/users', (req, res) => {
   db.query('SELECT * FROM users', (err, results) => {
     if (err) {
@@ -39,79 +64,12 @@ app.get('/api/users', (req, res) => {
   });
 });
 
-
-app.get('/api/users/:id', (req, res) => {
-  const userId = req.params.id;
-  db.query('SELECT * FROM users WHERE id = ?', [userId], (err, result) => {
-    if (err) {
-      console.error('Error fetching user by ID:', err);
-      return res.status(500).send('Error fetching user by ID');
-    }
-    if (result.length === 0) {
-      return res.status(404).send('User not found');
-    }
-    res.json(result[0]);
-  });
-});
-
-
-app.delete('/api/users/:id', (req, res) => {
-  const userIdToDelete = req.params.id;
-  db.query('DELETE FROM users WHERE id = ?', [userIdToDelete], (err, result) => {
-    if (err) {
-      console.error('Error deleting user:', err);
-      return res.status(500).send('Error deleting user');
-    }
-    if (result.affectedRows === 0) {
-      return res.status(404).send('User not found');
-    }
-    res.sendStatus(204); 
-  });
-});
-
-
-app.post('/api/users', (req, res) => {
-  const { name, surname, password, address } = req.body;
-  db.query(
-    'INSERT INTO users (name, surname, password, address) VALUES (?, ?, ?, ?)',
-    [name, surname, password, address],
-    (err, result) => {
-      if (err) {
-        console.error('Error inserting user:', err);
-        return res.status(500).send('Error inserting user');
-      }
-      const insertedUserId = result.insertId;
-      res.status(201).json({ id: insertedUserId });
-    }
-  );
-});
-
-
-app.post('/addUser', (req, res) => {
-  const { name, surname, password, address } = req.body;
-
-  
-  db.query(
-    'INSERT INTO users (name, surname, password, address) VALUES (?, ?, ?, ?)',
-    [name, surname, password, address],
-    (err, result) => {
-      if (err) {
-        console.error('Error inserting user:', err);
-        return res.status(500).send('Error inserting user');
-      }
-      console.log('User inserted');
-      res.redirect('/');
-    }
-  );
-});
-
-
+// Route to handle the HTML form submission for deleting a user
 app.post('/deleteUser', (req, res) => {
   const userIdToDelete = req.body.deleteId;
 
-  
   db.query(
-    'DELETE FROM users WHERE id = ?',
+    'DELETE FROM users WHERE ID = ?',
     [userIdToDelete],
     (err, result) => {
       if (err) {
@@ -123,6 +81,7 @@ app.post('/deleteUser', (req, res) => {
     }
   );
 });
+
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
--- sql and js/newFile.js
+++ sql and js/newFile.js
@@ -1,12 +1,12 @@
 const { app, db } = require('./app');
 
 app.post('/addUser', (req, res) => {
-  const { name, surname, password, address } = req.body;
-  console.log('Received form data:', { name, surname, password, address });
+  const { AD, SOYAD, PASSWORDS } = req.body;
+  console.log('Received form data:', { AD, SOYAD, PASSWORDS });
 
   db.query(
-    'INSERT INTO users (name, surname, password, address) VALUES (?, ?, ?, ?)',
-    [name, surname, password, address],
+    'INSERT INTO users (AD, SOYAD, PASSWORDS) VALUES (?, ?, ?)',
+    [AD, SOYAD, PASSWORDS],
     (err, result) => {
       if (err) {
         console.error('Error inserting user:', err);
